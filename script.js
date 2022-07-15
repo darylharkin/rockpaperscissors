7d677a9facdd96d7b52bb5cf534866a84bed599f
daryl <darylsharkin@gmail.com>
2022-07-15 21:41:44 +0100

    game keeps score and resets after 5 rounds

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function playerPlay() {
     let input = prompt('Choose your weapon. Rock, Paper or Scissors.');
     input = input.toLowerCase(); //make sure input is in the right format
     return playerSelection = input;
-    }
+}
 
 
 function computerPlay() { // make computer pick rock paper or scissors randomly
@@ -25,25 +28,48 @@ function playRound(playerSelection, computerSelection) { //make function that pl
     console.log(playerSelection);
     if (playerSelection === computerSelection) { //if player choice is equal to computer choice return tie
         alert('Tie Game!');
+        console.log('Computer has ' + computerScore + ' points.');
+        console.log('You have ' + playerScore + ' points.');
     } else if ( //computer wins
         (playerSelection == 'rock') && (computerSelection == 'paper') ||
         (playerSelection == 'paper') && (computerSelection == 'scissors') ||
         (playerSelection == 'scissors') && (computerSelection == 'rock')
     ) {
-        alert('You Lose!');
+        alert('You Lose! ' + computerSelection + ' beats ' + playerSelection + '!');
+        computerScore += 1;
+        console.log('Computer has ' + computerScore + ' points.');
+        console.log('You have ' + playerScore + ' points.');
+
     } else if ( //player wins
         (playerSelection == 'rock') && (computerSelection == 'scissors') ||
         (playerSelection == 'paper') && (computerSelection == 'rock') ||
         (playerSelection == 'scissors') && (computerSelection == 'paper')
     ) {
-        alert('You Win!');
+        alert('You Win! ' + playerSelection + ' beats ' + computerSelection + '!');
+        playerScore += 1;
+        console.log('Computer has ' + computerScore + ' points.');
+        console.log('You have ' + playerScore + ' points.');
     } else alert("That's not a weapon!");
 
+}
+
+function scoreReset() {
+    playerScore = 0;
+    computerScore = 0;
 }
 
 function game() {
     for (let i = 0; i < 5; i++) { //play 5 rounds
         playRound();
     }
+    if (playerScore == computerScore) {
+        alert("It's all square. Draw. Let's go again!");
+    } else if ( playerScore > computerScore) {
+        alert("You've beaten the computer! Well Done! Let's go again!");
+    } else if ( playerScore < computerScore) {
+        alert("Computer wins. You are a disgrace. Let's go again!")
+    }
+    scoreReset();
+    game();
 }
 game();
