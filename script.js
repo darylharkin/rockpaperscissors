971e900b8cc425e2834823e87c75759be426b6d8
daryl <darylsharkin@gmail.com>
2022-08-14 01:15:15 +0100

    Battlemessage, capitalising letters

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let battleMessage = ''
 
 function computerPlay() { // make computer pick rock paper or scissors randomly
     var computerSelection = Math.floor(Math.random() * 3) + 1; //picks a random number between 1 and 3
@@ -19,22 +20,25 @@ function playRound(playerSelection) { //make function that plays a round of rock
     console.log("computer played " + computerSelection);
     console.log("player played " + playerSelection);
     if (playerSelection === computerSelection) { //if player choice is equal to computer choice return tie
-        console.log('Tie Game!');
-        updateScoreboard();
+        battleMessage = 'Tie Game!';
+        updateBattleMessage();
     } else if ( //computer wins
         (playerSelection == 'rock') && (computerSelection == 'paper') ||
         (playerSelection == 'paper') && (computerSelection == 'scissors') ||
         (playerSelection == 'scissors') && (computerSelection == 'rock')
-    ) {
+        ) {
         console.log('You Lose! ' + computerSelection + ' beats ' + playerSelection + '!');
         computerScore += 1;
+        battleMessage = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        updateBattleMessage();
         updateScoreboard();
     } else if ( //player wins
         (playerSelection == 'rock') && (computerSelection == 'scissors') ||
         (playerSelection == 'paper') && (computerSelection == 'rock') ||
         (playerSelection == 'scissors') && (computerSelection == 'paper')
     ) {
-        console.log('You Win! ' + playerSelection + ' beats ' + computerSelection + '!');
+        battleMessage = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        updateBattleMessage();
         playerScore += 1;
         updateScoreboard();
     } 
@@ -44,6 +48,10 @@ function updateScoreboard() {
     document.getElementById("scoreboardCpu").textContent = "CPU: " + computerScore; 
     document.getElementById("scoreboardPlayer").textContent = "Player1: " + playerScore;
     checkScore();
+}
+
+function updateBattleMessage() {
+    document.getElementById("bmContainer").textContent = battleMessage;
 }
 
 function tieRound() {
@@ -67,9 +75,13 @@ function game() {
 }
 function checkScore() {
     if ( playerScore === 5) {
+        battleMessage = "Player1 Wins!"
+        updateBattleMessage();
         alert("You've beaten the computer! Well Done! Let's go again!");
         location.reload();
     } else if ( computerScore === 5) {
+        battleMessage = "CPU Wins!"
+        updateBattleMessage();
         alert("Computer wins. You are a disgrace. Let's go again!")
         location.reload();
     }
